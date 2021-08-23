e0037ea3905a51db8e5c39ca487f9265f0336a49
Rustam <rustam391@gmail.com>
2021-08-24 01:46:38 +0300

    Добавит prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from 'react'
 
 export const App = () => {
   return (
-  <div>
-    <h1>Game of life</h1>
-    <h2>Hello World</h2>
+    <div>
+      <h1>Game of life</h1>
+      <h2>Hello World</h2>
     </div>
-    )
+  )
 }
